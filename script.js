edc7852d65e6f47f4b27a4c23f7e24c6fc76b390
ArthurMelikyan <arthmelikyan@gmail.com>
2023-08-19 17:59:10 +0400

    updates

--- script.js
+++
@@ -1,5 +0,0 @@
-<!DOCTYPE html>
-<meta charset="utf-8">
-<title>Redirecting to https://arthmelikyan.github.io</title>
-<meta http-equiv="refresh" content="0; URL=https://arthmelikyan.github.io">
-<link rel="canonical" href="https://arthmelikyan.github.io"></link>
